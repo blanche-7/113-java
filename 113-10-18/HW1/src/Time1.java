3bf9c4611b27ea0649353d360957f08d4c3d0d7d
blanche-7 <a111222301@mail.shu.edu.tw>
2024-10-18 12:04:11 +0800

    113-10-18

---
+++ 113-10-18/HW1/src/Time1.java
@@ -0,0 +1,11 @@
+public class Time1 {
+    //時(hour)
+    //分(minute)
+    //秒(second)
+
+    //方法:建構子(設定時分秒的初始值，需檢查資料是否格是正確)
+    //方法是需要加入get/set
+    //方法:tick(時間增加一秒)
+    //方法:printUniversal(以24小時至輸出完整時間)
+    //方法:printStandard(以12小時至輸出完整時間)
+}
